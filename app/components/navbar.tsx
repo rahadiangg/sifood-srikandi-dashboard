1e63d459762bee1a17d953e3df12293da2a4572b
rahadiangg <dianraha11@gmail.com>
2023-07-17 02:39:53 +0700

    test

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -94,7 +94,7 @@ function Navbar() {
 
             <header className="bg-white shadow">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">SiFood Dashboard</h1>
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">SiFood Srikandi Dashboard</h1>
                 </div>
             </header>
         </>
